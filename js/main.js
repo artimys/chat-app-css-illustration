1e9df0436d8d584785037abbc9caa311125b5afc
Arturo Simon <artysimon@gmail.com>
2020-11-02 08:06:43 -0500

    Corrected fixed scroll height bug, Fixed by checking JS clientHeight

--- js/main.js
+++ js/main.js
@@ -2,12 +2,25 @@ const newMessageForm = document.getElementById("newMessageForm");
 const newMessageInput = document.getElementById("newMessage");
 const phoneMessageList = document.getElementById("phoneMessageList");
 
+let messagesHeightAssigned = false;
 
 newMessageForm.addEventListener("submit", function(e) {
     e.preventDefault();
 
     // Grab user input
     let = messageInput = newMessageInput.value;
+
+    // Set a few styles for messages container at first user input
+    if (messagesHeightAssigned == false) {
+        messagesHeightAssigned = true;
+
+        // Dynamically set height for messages container.
+        // off a few pixels in different browsers
+        phoneMessageList.style.height = phoneMessageList.clientHeight + "px";
+
+        // Display vertical scrollbar
+        phoneMessageList.style.overflowY = "scroll";
+    }
 
     // Proceed only if message is entered in input
     if (messageInput.length > 0) {
